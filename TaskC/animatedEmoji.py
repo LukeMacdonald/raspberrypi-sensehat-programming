0690dfc3ccb66748ffc87a7ed72697c28b258602
LukeMacdonald <lukemacdonald21@gmail.com>
2023-08-18 21:03:53 +1000

    added exception handling for task c to clear LED when program is excited

--- TaskC/animatedEmoji.py
+++ TaskC/animatedEmoji.py
@@ -1,12 +1,24 @@
-from sense_hat import SenseHat
 from time import sleep
-from emoji import EmojiList
+import sys
+import os
+# Get the path of the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+# Now you can import the module
+from extras.customSenseHat import CustomSenseHat
+from extras.emoji import EmojiList
+# Restoring sys.path to its original state (optional)
+sys.path.remove(parent_dir)
 
 if __name__ == '__main__':
-    sense = SenseHat()
+    sense = CustomSenseHat()
     emoji_list = EmojiList()
-    while True:
-        current_emoji = emoji_list.cycleEmoji()
-        # Display these colours on the LED matrix
-        sense.set_pixels(current_emoji.getPattern())
-        sleep(2)
+    try:
+        while True:
+            current_emoji = emoji_list.cycleEmoji()
+            # Display these colours on the LED matrix
+            sense.displayEmoji(current_emoji)
+            sleep(2)
+    except KeyboardInterrupt:
+            sense.clear()
--- TaskC/shakingEmoji.py
+++ TaskC/shakingEmoji.py
@@ -1,20 +1,23 @@
-from sense_hat import SenseHat
-from emoji import EmojiList
-
-def is_shaken(acceleration,shake_threshold):
-    x = abs(acceleration['x'])
-    y = abs(acceleration['y'])
-    z = abs(acceleration['z'])
-    return x > shake_threshold or y > shake_threshold or z > shake_threshold
+import sys
+import os
+# Get the path of the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+# Now you can import the module
+from extras.customSenseHat import CustomSenseHat
+from extras.emoji import EmojiList
+# Restoring sys.path to its original state (optional)
+sys.path.remove(parent_dir)
 
 if __name__ == '__main__':
-    sense = SenseHat()
+    sense = CustomSenseHat()
     emoji_list = EmojiList()
-    while True:
-        
-        acceleration = sense.get_accelerometer_raw()
-     
-        if is_shaken(acceleration, 2):
-            current_emoji = emoji_list.cycleEmoji()
-            # Display these colours on the LED matrix
-            sense.set_pixels(current_emoji.getPattern())
+    try:
+        while True:
+            if sense.detectShaking(2):
+                current_emoji = emoji_list.cycleEmoji()
+                # Display these colours on the LED matrix
+                sense.displayEmoji(current_emoji)
+    except KeyboardInterrupt:
+            sense.clear()

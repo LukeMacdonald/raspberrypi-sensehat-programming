89dae6e733972864d486cf0a791658bd87b41092
LukeMacdonald <lukemacdonald21@gmail.com>
2023-08-18 21:25:21 +1000

    improved code style for task a

--- TaskA/THNotifier.py
+++ TaskA/THNotifier.py
@@ -1,9 +1,9 @@
+# Import necessary libraries
 from datetime import datetime
 import os
 import time
 import json
 import sys
-
 
 # Get the path of the parent directory
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
@@ -12,75 +12,59 @@ sys.path.append(parent_dir)
 # Import modules from other directory
 from extras.customSenseHat import CustomSenseHat
 from extras.repository import Database
-
 # Restoring sys.path to its original state
 sys.path.remove(parent_dir)
 
-
 if __name__ == "__main__":
-        
-        myDb = Database()
-        sense = CustomSenseHat()
-        
-        with open('config.json', 'r') as f:
-            threshold_data = json.load(f)
-        try: 
-            while True:
-                
-                temperature = sense.getCalibratedTemperature()
-                humidity = sense.getHumditity()
-                # humid_color = (0,255,0)
-                # temp_color = (0, 255, 0)
-                # tempature_category = "normal"
-                # humidity_category = "normal"
+    # Initialize the database and Sense Hat objects
+    myDb = Database()
+    sense = CustomSenseHat()
 
-                tempature_output = round(temperature)
-                humidity_output = round(humidity)
-                
-                temp_color, temperature_category = sense.checkTemperatureThresold(
-                    tempature_output,
-                    int(threshold_data['cold_temperature_upper_limit']),
-                    int(threshold_data["hot_temperature_lower_limit"])
-                )
-                
-                humid_color, humidity_category = sense.checkHumidityThresold(
-                    humidity,
-                    int(threshold_data['dry_humidity_upper_limit']),
-                    int(threshold_data['wet_humidity_lower_limit'])
-                )
+    # Load configuration data from a JSON file
+    with open('config.json', 'r', encoding='utf-8') as f:
+        threshold_data = json.load(f)
 
-                # if humidity < int(threshold_data['dry_humidity_upper_limit']):
-                #     humid_color = (105, 50, 168)
-                #     humidity_category = "dry"
-                # elif humidity > int(threshold_data['wet_humidity_lower_limit']):
-                #     humid_color = (0,0,255)
-                #     humidity_category = "wet"
+    try:
+        while True:
+            # Get calibrated temperature and humidity readings
+            temperature = sense.get_calibrated_temperature()
+            humidity = sense.get_humidity()           
+            # Round temperature and humidity values
+            tempature_output = round(temperature)
+            humidity_output = round(humidity)
+            # Determine temperature category and associated color
+            temp_color, temperature_category = sense.check_temperature_threshold(
+                tempature_output,
+                int(threshold_data['cold_temperature_upper_limit']),
+                int(threshold_data["hot_temperature_lower_limit"])
+            )
+            # Determine humidity category and associated color
+            humid_color, humidity_category = sense.check_humidity_threshold(
+                humidity,
+                int(threshold_data['dry_humidity_upper_limit']),
+                int(threshold_data['wet_humidity_lower_limit'])
+            )
+            # Prepare data for database insertion
+            data = {
+                "recorded_time": str(datetime.now()),
+                "temperature": temperature,
+                "temperature_category": temperature_category,
+                "humidity": humidity,
+                "humidity_category": humidity_category
+            }
+            # Insert data into the database and retrieve records
+            myDb.insert(data)
+            myDb.select()
 
-                # if tempature_output  < int(threshold_data['cold_temperature_upper_limit']):
-                #     temp_color = (163,163,163)
-                #     tempature_category = "cold"
-                # elif tempature_output  > int(threshold_data["hot_temperature_lower_limit"]):
-                #     temp_color= (255,0,0)
-                #     tempature_category = "hot"
-                    
-                data = {
-                    "recorded_time": str(datetime.now()),
-                    "temperature": temperature,
-                    "temperature_category":temperature_category, 
-                    "humidity":humidity, 
-                    "humidity_category":humidity_category
-                }
-                
-                myDb.insert(data)
-                myDb.select()
-                
-                sense.displayMessage(f"T {str(tempature_output)}",temp_color,0.2)
-
-                # Wait for 5 seconds
-                time.sleep(5)
-                
-                sense.displayMessage(f"H {str(humidity_output)}",temp_color,0.2)
-                time.sleep(5)
-                sense.clear()
-        except KeyboardInterrupt:
+            # Display temperature on LED matrix and wait for 5 seconds
+            sense.display_message(f"T {str(tempature_output)}", temp_color, 0.2)
+            time.sleep(5)
+            # Display humidity on LED matrix and wait for 5 seconds
+            sense.display_message(f"H {str(humidity_output)}", temp_color, 0.2)
+            time.sleep(5)
+            # Clear the LED matrix
             sense.clear()
+    except KeyboardInterrupt:
+        # Clear the LED matrix in case of a keyboard interrupt
+        sense.clear()
+        
--- extras/customSenseHat.py
+++ extras/customSenseHat.py
@@ -3,53 +3,129 @@ from extras.emoji import Emoji
 from extras.utils import get_cpu_temp, get_smooth
 
 class CustomSenseHat:
+    """
+    A custom class that extends the functionality of the SenseHat module for Raspberry Pi.
+    """
+
     def __init__(self):
+        """
+        Initializes the CustomSenseHat object and connects to the Sense HAT hardware.
+        """
         self.sense = SenseHat()
-    
-    def calibrateTemperature(self,t1,t2):
-        t_cpu = get_cpu_temp()
-        t = (t1 + t2) / 2
-        t_corr = t - ((t_cpu - t) / 1.5)
-        return get_smooth(t_corr)
-        
-    def getCalibratedTemperature(self):
-        t1 = self.sense.get_temperature_from_humidity()
-        t2 = self.sense.get_temperature_from_pressure()
-        return self.calibrateTemperature(t1,t2)
-       
-    def getHumditity(self):
+
+    def calibrate_temperature(self, temperature1, temperature2):
+        """
+        Calibrates the temperature readings using CPU temperature and returns a smoothed result.
+
+        Parameters:
+        - temperature1 (float): Temperature reading from humidity sensor.
+        - temperature2 (float): Temperature reading from pressure sensor.
+
+        Returns:
+        float: Calibrated and smoothed temperature value.
+        """
+        t_cpu = get_cpu_temp()  # Assuming this function is defined somewhere
+        temperature = (temperature1 + temperature2) / 2
+        tempature_corr = temperature - ((t_cpu - temperature) / 1.5)
+        return get_smooth(tempature_corr)  # Assuming this function is defined somewhere
+
+    def get_calibrated_temperature(self):
+        """
+        Retrieves the calibrated temperature reading by averaging humidity and pressure sensor values.
+
+        Returns:
+        float: Calibrated temperature value.
+        """
+        temperature1 = self.sense.get_temperature_from_humidity()
+        temperature2 = self.sense.get_temperature_from_pressure()
+        return self.calibrate_temperature(temperature1, temperature2)
+
+    def get_humidity(self):
+        """
+        Retrieves the current humidity reading from the humidity sensor.
+
+        Returns:
+        float: Humidity value.
+        """
         return self.sense.get_humidity()
-    
-    def displayEmoji(self, emoji: Emoji):
+
+    def display_emoji(self, emoji: Emoji):
+        """
+        Displays an emoji pattern on the LED matrix.
+
+        Parameters:
+        emoji (Emoji): An instance of the Emoji class representing the desired emoji pattern.
+        """
         self.sense.set_pixels(emoji.getPattern())
-    
-    def detectShaking(self,shake_threshold):
+
+    def detect_shaking(self, shake_threshold):
+        """
+        Detects shaking motion based on accelerometer readings.
+
+        Parameters:
+        shake_threshold (float): Threshold value for accelerometer readings.
+
+        Returns:
+        bool: True if shaking is detected, False otherwise.
+        """
         acceleration = self.sense.get_accelerometer_raw()
-        x = abs(acceleration['x'])
-        y = abs(acceleration['y'])
-        z = abs(acceleration['z'])
-        return x > shake_threshold or y > shake_threshold or z > shake_threshold
-    
-    def displayMessage(self,message,colour,scoll_time):
-        self.sense.show_message(message,text_colour=colour,scroll_speed=scoll_time)
-     
+        x_axis = abs(acceleration['x'])
+        y_axis = abs(acceleration['y'])
+        z_axis = abs(acceleration['z'])
+        return x_axis > shake_threshold or y_axis > shake_threshold or z_axis > shake_threshold
+
+    def display_message(self, message, colour, scroll_time):
+        """
+        Displays a scrolling text message with specified text color and scroll speed.
+
+        Parameters:
+        message (str): The text message to be displayed.
+        colour (tuple): RGB tuple representing the text color.
+        scroll_time (float): Scroll speed of the message.
+        """
+        self.sense.show_message(message, text_colour=colour, scroll_speed=scroll_time)
+
     def clear(self):
+        """
+        Clears the LED matrix, turning off all pixels.
+        """
         self.sense.clear()
-    
-    def checkTemperatureThresold(self, temperature,low, high):
+
+    def check_temperature_threshold(self, temperature, low, high):
+        """
+        Compares the given temperature against low and high thresholds and returns status color 
+        and label.
+
+        Parameters:
+        temperature (float): Temperature value to be checked.
+        low (float): Lower temperature threshold.
+        high (float): Upper temperature threshold.
+
+        Returns:
+        tuple: RGB tuple representing the status color, and a string indicating the status label.
+        """
         if temperature < low:
-            return (163,163,163), "cold"
-        elif temperature > high:
-            return (255,0,0), "hot"
-        else:
-            return (0, 255, 0), "normal"
-    
-    def checkHumidityThresold(self, humidity,low, high):
+            return (163, 163, 163), "cold"
+        if temperature > high:
+            return (255, 0, 0), "hot"
+        return (0, 255, 0), "normal"
+
+    def check_humidity_threshold(self, humidity, low, high):
+        """
+        Compares the given humidity against low and high thresholds and returns status color 
+        and label.
+
+        Parameters:
+        humidity (float): Humidity value to be checked.
+        low (float): Lower humidity threshold.
+        high (float): Upper humidity threshold.
+
+        Returns:
+        tuple: RGB tuple representing the status color, and a string indicating the status label.
+        """
         if humidity < low:
             return (105, 50, 168), "dry"
-        elif humidity > high:
+        if humidity > high:
             return (0, 0, 255), "wet"
-        else:
-            return (0, 255, 0), "normal"
-    
+        return (0, 255, 0), "normal"
         

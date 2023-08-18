c9e9cffe58ae5051a75dfbcf0d98e3aa7dc0a9b8
LukeMacdonald <lukemacdonald21@gmail.com>
2023-08-18 21:04:50 +1000

    updated Task A to use oop and record data to database

--- TaskA/THNotifier.py
+++ TaskA/THNotifier.py
@@ -1,71 +1,86 @@
-from sense_hat import SenseHat
+from datetime import datetime
 import os
 import time
 import json
-
-# The below two functions were taken from lecture 4 to calibrate the temperature 
-# measured by the SenseHat
-
-# Get CPU temperature.
-def get_cpu_temp():
-    res = os.popen("vcgencmd measure_temp").readline()
-    return float(res.replace("temp=","").replace("'C\n",""))
-
-# Use moving average to smooth readings.
-def get_smooth(x):
-    if not hasattr(get_smooth, "t"):
-        get_smooth.t = [x,x,x]
-    
-    get_smooth.t[2] = get_smooth.t[1]
-    get_smooth.t[1] = get_smooth.t[0]
-    get_smooth.t[0] = x
-
-    return (get_smooth.t[0] + get_smooth.t[1] + get_smooth.t[2]) / 3
-
-with open('config.json', 'r') as f:
-    data = json.load(f)
-print(data['cold_temperature_upper_limit'])
+import sys
 
 
-sense = SenseHat()
-while True:
+# Get the path of the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# Add the parent directory to sys.path
+sys.path.append(parent_dir)
+# Import modules from other directory
+from extras.customSenseHat import CustomSenseHat
+from extras.repository import Database
 
-    t1 = sense.get_temperature_from_humidity()
-    t2 = sense.get_temperature_from_pressure()
-    t_cpu = get_cpu_temp()
-
-    h = sense.get_humidity()
-    p = sense.get_pressure()
-
-    # Calculates the real temperature compesating CPU heating.
-    t = (t1 + t2) / 2
-    t_corr = t - ((t_cpu - t) / 1.5)
-    t_corr = get_smooth(t_corr)
-    print(t_corr)
+# Restoring sys.path to its original state
+sys.path.remove(parent_dir)
 
 
-    humid_color = (0,255,0)
-    temp_color = (0, 255, 0)
+if __name__ == "__main__":
+        
+        myDb = Database()
+        sense = CustomSenseHat()
+        
+        with open('config.json', 'r') as f:
+            threshold_data = json.load(f)
+        try: 
+            while True:
+                
+                temperature = sense.getCalibratedTemperature()
+                humidity = sense.getHumditity()
+                # humid_color = (0,255,0)
+                # temp_color = (0, 255, 0)
+                # tempature_category = "normal"
+                # humidity_category = "normal"
 
-    tempature = int(t_corr)
-    humidity = int(h)
+                tempature_output = round(temperature)
+                humidity_output = round(humidity)
+                
+                temp_color, temperature_category = sense.checkTemperatureThresold(
+                    tempature_output,
+                    int(threshold_data['cold_temperature_upper_limit']),
+                    int(threshold_data["hot_temperature_lower_limit"])
+                )
+                
+                humid_color, humidity_category = sense.checkHumidityThresold(
+                    humidity,
+                    int(threshold_data['dry_humidity_upper_limit']),
+                    int(threshold_data['wet_humidity_lower_limit'])
+                )
 
-    if humidity < int(data['dry_humidity_upper_limit']):
-        humid_color = (105, 50, 168)
-    elif humidity > int(data['wet_humidity_lower_limit']):
-        humid_color = (0,0,255)
+                # if humidity < int(threshold_data['dry_humidity_upper_limit']):
+                #     humid_color = (105, 50, 168)
+                #     humidity_category = "dry"
+                # elif humidity > int(threshold_data['wet_humidity_lower_limit']):
+                #     humid_color = (0,0,255)
+                #     humidity_category = "wet"
 
-    if tempature  < int(data['cold_temperature_upper_limit']):
-       temp_color = (163,163,163)
-    elif tempature  > int(data["hot_temperature_lower_limit"]):
-        temp_color= (255,0,0)
+                # if tempature_output  < int(threshold_data['cold_temperature_upper_limit']):
+                #     temp_color = (163,163,163)
+                #     tempature_category = "cold"
+                # elif tempature_output  > int(threshold_data["hot_temperature_lower_limit"]):
+                #     temp_color= (255,0,0)
+                #     tempature_category = "hot"
+                    
+                data = {
+                    "recorded_time": str(datetime.now()),
+                    "temperature": temperature,
+                    "temperature_category":temperature_category, 
+                    "humidity":humidity, 
+                    "humidity_category":humidity_category
+                }
+                
+                myDb.insert(data)
+                myDb.select()
+                
+                sense.displayMessage(f"T {str(tempature_output)}",temp_color,0.2)
 
-
-
-    sense.show_message("T" + str(tempature),text_colour=temp_color,scroll_speed=0.2)
-
-    # Wait for 5 seconds
-    time.sleep(5)
-    sense.show_message("H" + str(humidity),text_colour=humid_color,scroll_speed=0.2)
-    time.sleep(5)
-    sense.clear()
+                # Wait for 5 seconds
+                time.sleep(5)
+                
+                sense.displayMessage(f"H {str(humidity_output)}",temp_color,0.2)
+                time.sleep(5)
+                sense.clear()
+        except KeyboardInterrupt:
+            sense.clear()
